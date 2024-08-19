c74f19ec1325f465767e6cf53c978d3c0bd05259
yzy-from-X1 <zongyuan1998@gmail.com>
2024-08-18 20:12:04 -0700

    first commit

---
+++ opener.py
@@ -0,0 +1,218 @@
+import os
+from dotenv import load_dotenv  
+import pandas as pd
+from datetime import datetime, timezone
+from binance.client import Client 
+from utils.trading_utils import *
+from binance.helpers import round_step_size
+import warnings
+
+warnings.filterwarnings("ignore", message="pandas only supports SQLAlchemy connectable")
+
+load_dotenv()
+DB_USERNAME = os.getenv('RDS_USERNAME') 
+DB_PASSWORD = os.getenv('RDS_PASSWORD') 
+DB_HOST = os.getenv('RDS_ENDPOINT') 
+DB_NAME = 'financial_data'
+
+api_key = os.getenv('BINANCE_API')  
+api_secret = os.getenv('BINANCE_SECRET')  
+client = Client(api_key, api_secret)
+
+# get from sql
+conn = connect_to_db(DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD)
+query = f"""
+with key_pairs as (
+    select *, row_number() over (partition by symbol order by date desc) as rn
+    from coin_historical_price
+),
+key_pairs_120d as (
+    select *
+    from key_pairs
+    where rn <= 120
+),
+ols_spread as (
+    select a.date, a.symbol as symbol_a, b.symbol as symbol_b,
+    a.close as close_a, b.close as close_b, 
+    a.close - c.ols_coeff * b.close as ols_spread, c.*
+    from key_pairs_120d a 
+    join key_pairs_120d b
+    on a.date = b.date
+    join coin_signal c
+    on c.symbol1 = a.symbol and c.symbol2 = b.symbol
+),
+bb_band as (
+    select *,
+    coalesce(avg(ols_spread) over (partition by symbol_a, symbol_b order by date rows between 19 preceding and current row), ols_spread) as sma,--ADJUSTABLE
+    coalesce(stddev(ols_spread) over (partition by symbol_a, symbol_b order by date rows between 19 preceding and current row), 0) as sd--ADJUSTABLE
+    from ols_spread
+),
+ranked_results as (
+    select 
+    symbol_a, symbol_b, date,
+	round(close_a, 2) as close_a, round(close_b, 2) as close_b,
+	round(ols_spread, 2) as ols_spread,
+	round(most_recent_coint_pct, 2) as most_recent_coint_pct, 
+    round(recent_coint_pct, 2) as recent_coint_pct, 
+    round(hist_coint_pct, 2) as hist_coint_pct, 
+	round(r_squared, 2) as r_squared, 
+    round(ols_constant, 2) as ols_constant, 
+    round(ols_coeff, 3) as ols_coeff,  
+    round(((ols_spread - sma)/nullif(2 * sd, 0)) * 100, 0) as key_score,
+	case 
+        when abs(ols_coeff) < 1 then round(close_a/abs(ols_coeff) + close_b, 2)
+        else round(close_a + abs(ols_coeff)*close_b, 2)
+    end as investment,
+	round(abs(ols_spread - sma), 2) as potential_win,
+	round(sma, 2) as rolling_mean,
+	round(sma + 1.8 * sd, 2) as upper_band, round(sma - 1.8 * sd, 2) as lower_band,
+    row_number() over (partition by symbol_a, symbol_b order by date desc) as rn
+    from bb_band
+)
+select symbol_a, symbol_b, date, 
+most_recent_coint_pct, recent_coint_pct, hist_coint_pct, 
+r_squared, ols_constant, ols_coeff, 
+round(potential_win/nullif(investment, 0), 4) as potential_win_pct,
+key_score, investment, potential_win
+from ranked_results
+where rn = 1 and potential_win/nullif(investment, 0) >= {MIN_POTENTIAL_WIN_PCT}
+and most_recent_coint_pct >= {MIN_RECENT_COINT} 
+and r_squared >= {MIN_R_SQUARED}  
+order by most_recent_coint_pct desc, recent_coint_pct desc, 
+hist_coint_pct desc, potential_win_pct desc;
+"""
+monitored_pairs_df = pd.read_sql(query, conn)
+conn.close()
+
+# reading saving files
+if os.path.exists(strat_csv_file):
+    strat_df = pd.read_csv(strat_csv_file)
+    if strat_df.empty:
+        strat_df = pd.DataFrame()
+else:
+    strat_df = pd.DataFrame()
+if os.path.exists(order_csv_file):
+    orders_df = pd.read_csv(order_csv_file)
+    if orders_df.empty:
+        orders_df = pd.DataFrame()
+else:
+    orders_df = pd.DataFrame()
+
+for index, row in monitored_pairs_df.iterrows():
+   symbol_Y = row['symbol_a']+'USDT'
+   symbol_X = row['symbol_b']+'USDT'
+   ols_coeff = row['ols_coeff']
+   ols_constant = row['ols_constant']
+   print(f"\n--- Monitoring {symbol_Y} X {symbol_X} with coeff:{ols_coeff} and constant:{ols_constant}...")
+
+   '''Check whether pair already traded or stop loss closed'''
+   already_traded = False
+   for _, order_row in orders_df.iterrows():
+      order_pair_set = {order_row['symbol_Y'], order_row['symbol_X']}
+      if {symbol_Y, symbol_X} == order_pair_set and order_row['pair_trade_status'] in {"OPEN", "UPPER_STOPPED", "LOWER_STOPPED"}:
+         already_traded = True
+         print(f"ALREADY TRADED with status: {order_row['pair_trade_status']}.")
+         break
+   if already_traded:
+      continue
+   
+   '''Monitoring for opens'''
+   # 1. connect get latest real time data.
+   try:
+      Y_minute_data, Y_daily_data = get_bn_data(client, symbol_Y)
+   except Exception as e:
+      print(f"No Data for {symbol_Y} on Binance: {str(e)}")  
+      continue
+   try:
+      X_minute_data, X_daily_data = get_bn_data(client, symbol_X)
+   except Exception as e:
+      print(f"No Data for {symbol_X} on Binance: {str(e)}")  
+      continue
+   
+   # in case time difference make data different length
+   if len(Y_minute_data) != len(X_minute_data):
+      print(f"Length mismatch between Y_minute_data ({len(Y_minute_data)}) and X_minute_data ({len(X_minute_data)})")
+      continue  
+
+   if len(Y_daily_data) != len(X_daily_data):
+      print(f"Length mismatch between Y_daily_data ({len(Y_daily_data)}) and X_daily_data ({len(X_daily_data)})")
+      continue
+   
+   # 2. calculate spread.
+   minute_spread = calculate_spread(Y_minute_data, X_minute_data, ols_coeff, ols_constant)
+   daily_spread_range = calculate_bollinger_bands(calculate_spread(Y_daily_data, X_daily_data, ols_coeff, ols_constant), BB_BAND_WINDOW, BB_SIGNAL_STD_MULT, BB_STOPLOSS_STD_MULT).drop(columns=['spread'])
+
+   minute_spread['date_only'] = minute_spread['date'].dt.date
+   min_spread_w_day_band = pd.merge(minute_spread, daily_spread_range, left_on='date_only', right_on=daily_spread_range['date'].dt.date, how='left')
+   min_spread_w_day_band = min_spread_w_day_band.drop(columns=['date_only', 'date_y'])
+   min_spread_w_day_band.rename(columns={'date_x': 'date'}, inplace=True)
+
+   # 3. trade based on the spread. long x and short y.
+   latest_min = min_spread_w_day_band.iloc[-1]
+   if latest_min['spread'] > latest_min['upper_band']:
+      strat = 'short Y long X'
+   elif latest_min['spread'] < latest_min['lower_band']:
+      strat = 'long Y short X'
+   else:
+      strat = 'stand_by'
+      
+   latest_strat = pd.DataFrame([{'date': latest_min['date'], 
+                                 'symbol_Y':symbol_Y, 
+                                 'symbol_X':symbol_X,
+                                 'strategy': strat}])
+   strat_df = pd.concat([strat_df, latest_strat], ignore_index=True)
+   strat_df.to_csv(strat_csv_file, index=False)
+   
+   '''Execute trade at prime condition'''
+   if latest_strat['strategy'].iloc[-1] != 'stand_by':
+      print(f"Executing a trade for {symbol_Y}X{symbol_X}...")
+      symbol_Y = latest_strat['symbol_Y'].iloc[-1]
+      symbol_X = latest_strat['symbol_X'].iloc[-1]
+
+      # determine order size with formula
+      curr_price_Y = round_step_size(Y_minute_data['close'].iloc[-1], 0.00001)
+      curr_price_X = round_step_size(X_minute_data['close'].iloc[-1], 0.00001)
+
+      # set tick size based on crypto price
+      y_tick_size, x_tick_size = get_tick_size(curr_price_Y, curr_price_X)
+
+      amt_Y = round_step_size(TOTAL_USDT_PER_TRADE/(curr_price_X*ols_coeff + curr_price_Y), y_tick_size)
+      amt_X = round_step_size(ols_coeff*amt_Y, x_tick_size)
+
+      usdt_on_Y = round_step_size(amt_Y*curr_price_Y, y_tick_size)
+      usdt_on_X = round_step_size(amt_X*curr_price_X , x_tick_size)
+      print("curr_price_Y, amt_Y, curr_price_X, amt_X, usdt_on_Y, usdt_on_X")
+      print(curr_price_Y, amt_Y, curr_price_X, amt_X, usdt_on_Y, usdt_on_X)
+
+      if latest_strat['strategy'].iloc[-1] == 'long Y short X':
+            # long Y
+            long_order = client.order_market_buy(symbol=symbol_Y, quantity=amt_Y)
+            print(f'longed {symbol_Y}. bought {amt_Y} of them of ${amt_Y*curr_price_Y}')
+            
+            # borrow X
+            short_loan = client.create_margin_loan(asset=symbol_X.replace('USDT', ''), amount=str(amt_X))
+            print(short_loan)
+            # short X
+            short_order = client.create_margin_order(symbol=symbol_X, side=SIDE_SELL,type=ORDER_TYPE_MARKET, quantity=amt_X)
+            print(f'shorted {symbol_X}. short sold {amt_X} of them of ${amt_X*curr_price_X}')
+            
+            orders_df = pd.concat([orders_df, pairs_order_to_pd_df("OPEN", ols_coeff, ols_constant, long_order, short_order, short_loan, symbol_Y, symbol_X)])
+            
+      elif latest_strat['strategy'].iloc[-1] == 'short Y long X':
+            # long X
+            long_order = client.order_market_buy(symbol=symbol_X, quantity=amt_X)
+            print(f'longed {symbol_X}. bought {amt_X} of them of ${amt_X*curr_price_X}')
+            
+            # borrow Y
+            short_loan = client.create_margin_loan(asset=symbol_Y.replace('USDT', ''), amount=str(amt_Y))
+            # short Y
+            short_order = client.create_margin_order(symbol=symbol_Y, side=SIDE_SELL,type=ORDER_TYPE_MARKET, quantity=amt_Y)
+            print(f'shorted {symbol_Y}. short sold {amt_Y} of them of ${amt_Y*curr_price_Y}')
+
+            orders_df = pd.concat([orders_df, pairs_order_to_pd_df("OPEN", ols_coeff, ols_constant, long_order, short_order, short_loan, symbol_Y, symbol_X)])
+               
+      orders_df.to_csv(order_csv_file, index=False)
+      print(f'Updated the new order to {order_csv_file}!')
+   else:
+      print(f"NO TRADE in current minute candle.")
+   
---
+++ utils/trading_utils.py
@@ -0,0 +1,184 @@
+import pandas as pd
+from datetime import datetime
+from binance.client import Client
+from datetime import datetime, timezone
+from binance.client import Client 
+from binance.enums import *
+import psycopg2
+from psycopg2 import OperationalError
+
+# pairs selection from sql criteria
+MIN_RECENT_COINT = 0.8
+MIN_R_SQUARED = 0.7
+MIN_POTENTIAL_WIN_PCT = 0.01
+
+# trade params
+TOTAL_USDT_PER_TRADE = 20
+BB_BAND_WINDOW = 20 
+BB_SIGNAL_STD_MULT = 1.8
+BB_STOPLOSS_STD_MULT = 2.8
+
+# file location
+strat_csv_file = './strat_df.csv'
+order_csv_file = './order_df.csv'
+
+def connect_to_db(DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD):
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USERNAME,
+            password=DB_PASSWORD)
+        print(f"Connected to {DB_HOST} {DB_NAME}!")
+        return conn
+    except OperationalError as e:
+        print(f"{e}")
+        return None
+    
+def candle_transformation(candle):
+      output = []
+      for entry in candle:
+         date = datetime.fromtimestamp(entry[0] / 1000, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+         open_price = entry[1]
+         high_price = entry[2]
+         low_price = entry[3]
+         close_price = entry[4]
+         volume = entry[5]
+         output.append([date, open_price, high_price, low_price, close_price, volume])
+
+      output_df = pd.DataFrame(output, columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+      output_df['date'] = pd.to_datetime(output_df['date'])
+      output_df['open'] = output_df['open'].astype(float)
+      output_df['high'] = output_df['high'].astype(float)
+      output_df['low'] = output_df['low'].astype(float)
+      output_df['close'] = output_df['close'].astype(float)
+      output_df['volume'] = output_df['volume'].astype(float)
+
+      return output_df
+
+def get_bn_data(client, symbol): 
+      minute_candles = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE)
+      minute_data = candle_transformation(minute_candles)
+ 
+      daily_candle = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1DAY, "120 day ago UTC")
+      daily_data = candle_transformation(daily_candle)
+      return minute_data, daily_data
+
+def get_tick_size(curr_price_Y, curr_price_X):
+   if curr_price_Y < 5:
+      y_tick_size = 1
+   elif curr_price_Y >=5 and curr_price_Y < 100:
+      y_tick_size = 0.01
+   elif curr_price_Y >=100 and curr_price_Y < 2000:
+      y_tick_size = 0.001  
+   elif curr_price_Y >=1000 and curr_price_Y < 10000:
+      y_tick_size = 0.0001 
+   elif curr_price_Y >=10000:
+      y_tick_size = 0.00001 
+
+   if curr_price_X < 5:
+      x_tick_size = 1
+   elif curr_price_X >=5 and curr_price_X < 100:
+      x_tick_size = 0.01
+   elif curr_price_X >=100 and curr_price_X < 2000:
+      x_tick_size = 0.001  
+   elif curr_price_X >=1000 and curr_price_X < 10000:
+      x_tick_size = 0.0001 
+   elif curr_price_X >=10000:
+      x_tick_size = 0.00001 
+   return y_tick_size, x_tick_size
+
+def pairs_order_to_pd_df(pair_trade_status, ols_coeff, ols_constant, long_order, short_order, short_loan, symbol_Y, symbol_X):
+   
+   short_order_tranx_time_str = datetime.fromtimestamp(short_order['transactTime'] / 1000).strftime('%Y-%m-%d %H:%M:%S')
+   long_order_tranx_time_str = datetime.fromtimestamp(long_order['transactTime'] / 1000).strftime('%Y-%m-%d %H:%M:%S')
+   
+   short_order_coins_quantity = float(short_order['executedQty'])
+   long_order_coins_quantity = float(long_order['executedQty'])
+   
+   short_order_usdt_amt = float(short_order['cummulativeQuoteQty'])
+   long_order_usdt_amt = float(long_order['cummulativeQuoteQty'])
+    
+   return pd.DataFrame([{
+      'pair_trade_status': pair_trade_status,
+      'symbol_Y': symbol_Y,
+      'symbol_X': symbol_X,
+      'long_symbol': long_order['symbol'],
+      'long_time': long_order_tranx_time_str,
+      'long_side': long_order['side'],
+      'long_quantity': long_order_coins_quantity,
+      'long_usdt_amt': long_order_usdt_amt,
+      'long_status': long_order['status'],
+      'long_orderId': str(long_order['orderId']),
+      'long_clientOrderId': str(long_order['clientOrderId']),
+      
+      'short_symbol': short_order['symbol'],
+      'short_time': short_order_tranx_time_str,
+      'short_side': short_order['side'],
+      'short_quantity': short_order_coins_quantity,
+      'short_usdt_amt': short_order_usdt_amt,
+      'short_status': short_order['status'],
+      'short_loanId': str(short_loan['tranId']),
+      'short_orderId': str(short_order['orderId']),
+      'short_clientOrderId': str(short_order['clientOrderId']),
+      'ols_coeff': ols_coeff,
+      'ols_constant': ols_constant
+   }])
+
+def calculate_bollinger_bands(df, window, std_dev, stop_loss_std_dev, col_name='spread'):
+    df['rolling_mean'] = df[col_name].rolling(window=window).mean()
+    df['rolling_std'] = df[col_name].rolling(window=window).std()
+    df['upper_band'] = df['rolling_mean'] + (df['rolling_std'] * std_dev)
+    df['lower_band'] = df['rolling_mean'] - (df['rolling_std'] * std_dev)
+    df['upper_stop_loss'] = df['rolling_mean'] + (df['rolling_std'] * stop_loss_std_dev)
+    df['lower_stop_loss'] = df['rolling_mean'] - (df['rolling_std'] * stop_loss_std_dev)
+    return df
+
+def calculate_spread(df1, df2, ols_coeff, ols_constant=0):
+    df1 = df1.set_index('date')
+    df2 = df2.set_index('date')
+    spread = df1['close'] - ols_constant - ols_coeff * df2['close']
+    
+    result_df = pd.DataFrame({
+        'date': df1.index,
+        'spread': spread
+    }).reset_index(drop=True)
+    return result_df 
+
+def order_to_pd_df(order, symbol_Y, symbol_X, margin_type):
+   transact_time_dt = datetime.fromtimestamp(order['transactTime'] / 1000)
+   transact_time_str = transact_time_dt.strftime('%Y-%m-%d %H:%M:%S')
+   coins_quantity = float(order['executedQty'])
+   usdt_amt = float(order['cummulativeQuoteQty'])
+   if order['side'] == 'SELL':
+      coins_quantity = -coins_quantity
+   if order['side'] == 'BUY':
+      usdt_amt = -usdt_amt
+        
+   return  pd.DataFrame([{
+      'symbol_Y': symbol_Y,
+      'symbol_X': symbol_X,
+      'margin': margin_type,
+      'side': order['side'],
+      'symbol': order['symbol'],
+      'datetime': transact_time_str,
+      'coins_quantity': coins_quantity,
+      'usdt_amt': usdt_amt,
+      'status': order['status'],
+      'type': order['type'],
+      'orderId': order['orderId'],
+      'clientOrderId': order['clientOrderId']
+   }])
+
+# KLINE_INTERVAL_1MINUTE 
+# KLINE_INTERVAL_3MINUTE 
+# KLINE_INTERVAL_5MINUTE 
+# KLINE_INTERVAL_15MINUTE 
+# KLINE_INTERVAL_30MINUTE 
+# KLINE_INTERVAL_1HOUR 
+# KLINE_INTERVAL_2HOUR 
+# KLINE_INTERVAL_4HOUR 
+# KLINE_INTERVAL_6HOUR 
+# KLINE_INTERVAL_8HOUR 
+# KLINE_INTERVAL_12HOUR 
+# KLINE_INTERVAL_1DAY 
